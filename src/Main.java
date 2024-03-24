ee87e0cb96300218b78cbd45b286205342386491
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-24 00:40:34 -0300

    Enunciado creado.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+/*
+* 11. Ingresar el tiempo en segundos que realizó el ganador de la clasificación
+* de una carrera de F1. Luego ingresar los tiempos de los otros 9 corredores e
+* informar cuántos disputarán la carrera.
+* Para que un corredor pueda participar su tiempo de clasificación no puede superar
+* n 15 % al del ganador.
+* */
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
